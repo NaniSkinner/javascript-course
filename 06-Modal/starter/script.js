e95116d48c82b32f15c212b9c3d89003a0659ae4
Nani Skinner <nanis@MacBook-Air.local>
2024-08-15 11:34:36 -0500

    Updating with Github Command Comments

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -10,3 +10,13 @@ for (let i = 0; i < btnsOpenMoal.lenght; i++)
   btnsOpenMoal[i].addEventListener('click', function () {
     console.log('Button clicked');
   });
+
+// This comment is just to show an example of updating a repo.
+
+// 1. After we make a change, we need to make sure we are in the right file path within the terminal
+
+// 2. Once we've done that, we'll then update the github repo by running these commands:
+
+// `git add .`
+// `git commit -m "Updating with Github Command Comments"
+// `git push`
