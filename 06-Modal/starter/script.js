2d0fc446df724cc6d7da0b63d9326941ae82c7f9
Nani Skinner <nanis@MacBook-Air.local>
2024-08-17 17:52:09 -0500

    adding click and removing hidden modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,20 +3,11 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenMoal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenMoal);
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenMoal.lenght; i++)
-  btnsOpenMoal[i].addEventListener('click', function () {
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button clicked');
+    modal.classList.remove('hidden');
   });
-
-// This comment is just to show an example of updating a repo.
-
-// 1. After we make a change, we need to make sure we are in the right file path within the terminal
-
-// 2. Once we've done that, we'll then update the github repo by running these commands:
-
-// `git add .`
-// `git commit -m "Updating with Github Command Comments"
-// `git push`
