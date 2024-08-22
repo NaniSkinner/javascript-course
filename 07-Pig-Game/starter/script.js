ca92ab888f79b151f48ad2e908e94b474c2b5f41
NaniSkinner <skinner.galvez@gmail.com>
2024-08-22 11:31:17 -0500

    codding the acction hold btn and switching playes, holding score, ending game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -25,27 +25,63 @@ diceEl.classList.add('hidden'); // add the hidden dice command
 const scores = [0, 0]; // big scrores of the game stored as arrays (arrays are 0 based that is whhy player is 0 and 1)
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true; // boolean that states the game is being played. If false it does not allow to play the game.
+
+const switchPlayer = function () {
+  // no peramiter needed since the code is the exact same in both scenarios
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active'); // toggle-> add the class if is not there and remove it if is there
+  player1El.classList.toggle('player--active'); // re assingng player, checking if player is 0 and changing to 1
+};
 
 // Reacting to clicking the roll botton. Rolling Dice functionality
 btnRoll.addEventListener('click', function () {
-  //1. Generating a ramdom dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1; // dice is here b/c we want to generate a new number each roll. Trunc removes any decimal to a whole.
-  console.log(dice);
-  //2. Display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`; // imagen name, to load an emagen of the dice
-  //3. Check for rolled 1; if true, switch to next player
-  if (dice !== 1) {
-    // add dice to current score
-    currentScore += dice; // += means current = current
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore; // set score on active player
-  } else {
-    //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0; // re assingng player, checking if player is 0 and changing to 1
-    player0El.classList.toggle('player--active'); // toggle-> add the class if is not there and remove it if is there
-    player1El.classList.toggle('player--active');
+  if (playing) {
+    // boolean and no need to write any condition
+    //1. Generating a ramdom dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1; // dice is here b/c we want to generate a new number each roll. Trunc removes any decimal to a whole.
+
+    //2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`; // imagen name, to load an emagen of the dice
+    //3. Check for rolled 1; if true, switch to next player
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice; // += means current = current
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore; // set score on active player
+    } else {
+      //switch to next player
+      switchPlayer(); // calling the function
+    }
+  }
+});
+
+// have an acction once the btn hold is clicked
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //1. Add current score to active player's score
+    scores[activePlayer] += currentScore; // score at current player = plus the current score ---> score[1] = score[1] + currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]; // dynamicly replacing by `` template string
+
+    //2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+      //Finish the game
+      playing = false;
+      diceEl.classList.add('hidden'); // removing the dice once game is won!
+      document
+        .querySelector(`.player--${activePlayer}`) // need a . selecting a class. Elements by ID no need for .
+        .classList.add('player--winner'); // select the player -- add class name
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active'); // remove active player and display only the winner
+    } else {
+      //Switch player
+      switchPlayer();
+    }
   }
 });
