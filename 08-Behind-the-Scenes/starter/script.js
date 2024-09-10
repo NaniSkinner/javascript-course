ed6815c627e5203dc2447412c73f17804f6af6fe
NaniSkinner <skinner.galvez@gmail.com>
2024-09-10 13:17:18 -0500

    took notes and completed lesson

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -11,15 +11,18 @@ function calcAge(birthYear) {
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var milenial = true;
+      // Creating NEW variable w/ same name as outer scope's variable
       const firstName = 'Steven'; // variable name in the current scope first (parent scope)
+
+      //Reassining outer scope's variable
+      outPut = 'NEW OUTPUT'; //manipulated an existing variable inside a scope, reassining
+
       const str = `Oh, and you are a milenial, ${firstName}`;
       console.log(str);
 
       function add(a, b) {
         return a + b; //blocked scoped
       }
-
-      outPut = 'NEW OUTPUT'; //manipulated an existing variable inside a scope, reassining
     }
     console.log(milenial); // variables with var are function scopped. They ignore the block
     console.log(outPut); // works w/out strict mode
