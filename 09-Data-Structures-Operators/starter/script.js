a90aceecfce84b30c235167479c2db15fdfd2123
NaniSkinner <skinner.galvez@gmail.com>
2024-11-05 11:55:00 -0500

    review the challange notes

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -623,8 +623,8 @@ const game = {
 
 //Task #1 Create one player array for each team (variables 'players1' and 'players2') It is aksing to split the players into each teams without listing them side by side. ARRAY DISTRUCTION 
 
-const [players1, players2] = game.players 
-console.log(players1, players2);
+const [players1, players2] = game.players; 
+console.log(players1, players2); 
 
 // Task 2 The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players ARRAY DISTRUCTION 
 
@@ -641,7 +641,7 @@ console.log(allPlayers);
 
 const players1Final = [ ...players1, 'Thiago', 'Coutinho', 'Perisic']; 
 
-//Task 5 : Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2') ONJECT DESTRUCTURING 
+//Task 5 : Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2') OBJECT DESTRUCTURING (: is to change the name for what is wanted)
 const { team1, x: draw, team2} = game.odds; 
 
 // Task 6 : Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in) TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored  REST PARAMETER 
@@ -652,7 +652,7 @@ function printGoals( ...players) {
   console.log(`Total goals scored ${players.length}`);
 }
 
-// Task 7: The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+// Task 7: The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator. AND OPERATOR ( it continues the sircuit once is true)
 
 team1 < team2 && console.log('Team 1 is more likely to win');
 team1 > team2 && console.log('Team 2 is more likely to win');
